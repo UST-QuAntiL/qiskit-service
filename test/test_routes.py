179f4804a06c243d10b0926dc3bcc3504b5abee9
TimPhillip <timphillip.schneider@t-online.de>
2020-07-24 14:06:46 +0200

    Using unittest framework now.

--- test/test_routes.py
+++ test/test_routes.py
@@ -1,65 +1,18 @@
-import os
-import tempfile
-import pytest
-
-from app import Config, db
-from app.result_model import Result
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from redis import Redis
-import rq
-from flask import Flask
+import unittest
+from app import app, db
 
 
-@pytest.fixture(scope='module')
-def test_client():
+class RoutesTestCase(unittest.TestCase):
 
-    flask_app = Flask(__name__)
-    flask_app.config.from_object(Config)
+    def test_version(self):
 
-    flask_app.config['BCRYPT_LOG_ROUNDS'] = 4
-    flask_app.config['TESTING'] = True
-    flask_app.config['WTF_CSRF_ENABLED'] = False
+        test_client = app.test_client()
+        response = test_client.get('/qiskit-service/api/v1.0/version')
 
-    db = SQLAlchemy(flask_app)
-    migrate = Migrate(flask_app, db)
+        self.assertEqual(response.status_code, 200)
+        json_data = response.get_json()
+        self.assertTrue("version" in json_data)
+        self.assertEqual(json_data['version'], "1.0")
 
-    flask_app.redis = Redis.from_url(flask_app.config['REDIS_URL'])
-    flask_app.execute_queue = rq.Queue('qiskit-service_execute', connection=flask_app.redis, default_timeout=3600)
-
-    test_client = flask_app.test_client()
-
-    context = flask_app.app_context()
-    context.push()
-
-    yield test_client
-
-    context.pop()
-
-
-@pytest.fixture()
-def add_dummy_results():
-
-    result = Result(id="0")
-    db.session.add(result)
-    db.session.commit()
-
-
-def test_version(test_client):
-
-    response = test_client.get('/qiskit-service/api/v1.0/version')
-
-    assert response.status_code == 200
-    assert 'version' in response.data
-    assert response.data['version'] == '1.0'
-
-
-def test_get_result(test_client):
-
-    result_id = "0"
-    response = test_client.get('/qiskit-service/api/v1.0/results/%s/' % result_id)
-
-    assert response.status_code == 200
-
-
-
+if __name__ == "__main__":
+    unittest.main()
