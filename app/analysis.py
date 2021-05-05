f0aa65cfcecf15d9585168d9990ea2ae8a98641b
pilotfritz98 <54139358+pilotfritz98@users.noreply.github.com>
2021-05-05 09:37:08 +0200

    Updated analysis of benchmarks

---
+++ app/analysis.py
@@ -0,0 +1,61 @@
+import math
+
+import scipy
+
+
+def calc_expected_value(counts_dict):
+    result = 0
+    for key in counts_dict.keys():
+        result += int(key, 2) * counts_dict[key]
+    return result
+
+
+def calc_standard_deviation(counts_dict, expected_value):
+    sum = 0
+    for key in counts_dict.keys():
+        sum += ((int(key, 2) - expected_value) ** 2) * counts_dict[key]
+    return math.sqrt(sum)
+
+
+def calc_percentage_error(counts_sim, counts_real):
+    result = {}
+    for key in counts_sim.keys():
+        if key in counts_real.keys():
+            error = (counts_sim[key] - counts_real[key]) / counts_sim[key]
+            result[key] = abs(error)
+    return result
+
+
+def calc_intersection(counts_sim, counts_real, shots):
+    intersection = 0
+    for key in counts_real.keys():
+        if key not in counts_sim.keys():
+            counts_sim[key] = 0
+    for key in counts_sim.keys():
+        if key not in counts_real.keys():
+            counts_real[key] = 0
+        intersection = intersection + min(counts_sim[key], counts_real[key])
+    return intersection / shots
+
+
+
+def calc_correlation(counts_sim, counts_real, shots):
+    for key in counts_real.keys():
+        if key not in counts_sim.keys():
+            counts_sim[key] = 0
+    for key in counts_sim.keys():
+        if key not in counts_real.keys():
+            counts_real[key] = 0
+    sum_combined = 0
+    sum_sim = 0
+    sum_real = 0
+    h_sim = shots / len(counts_sim)
+    h_real = shots / len(counts_real)
+    for key in counts_real.keys():
+        sum_real = sum_real + ((counts_real[key] - h_real) ** 2)
+    for key in counts_sim.keys():
+        sum_sim = sum_sim + ((counts_sim[key] - h_sim) ** 2)
+        sum_combined = sum_combined + ((counts_sim[key] - h_sim) * (counts_real[key]- h_real))
+    correlation = sum_combined / (math.sqrt(sum_sim * sum_real))
+
+    return correlation
--- app/benchmarking.py
+++ app/benchmarking.py
@@ -1,12 +1,15 @@
 import json
+import math
 
 from flask import request, abort, jsonify
 from qiskit import IBMQ, assemble, transpile
 from qiskit.circuit.random import random_circuit
+from qiskit.converters import circuit_to_dag
 from qiskit.providers.jobstatus import JOB_FINAL_STATES
 from qiskit.providers import JobError
+from qiskit.transpiler.passes import RemoveFinalMeasurements
 
-from app import app, db, parameters, ibmq_handler
+from app import app, db, parameters, ibmq_handler, analysis
 from app.benchmark_model import Benchmark
 from app.result_model import Result
 
@@ -45,12 +48,17 @@ def randomize(qpu_name, num_of_qubits, shots, min_depth_of_circuit, max_depth_of
             rowcount = db.session.query(Benchmark).count()
             benchmark_id = rowcount // 2
             qx = random_circuit(num_qubits=num_of_qubits, depth=i, measure=True)
-            qcircuit_sim = transpile(qx, backend=backend_sim)
-            qcircuit_real = transpile(qx, backend=backend_real)
+            qcircuit_sim = transpile(qx, backend=backend_sim, optimization_level=3)
+            qcircuit_real = transpile(qx, backend=backend_real, optimization_level=3)
+            remove_final_meas = RemoveFinalMeasurements()
+            active_qubits_real = [
+                qubit for qubit in qcircuit_real.qubits if
+                qubit not in remove_final_meas.run(circuit_to_dag(qcircuit_real)).idle_wires()
+            ]
             transpiled_depth_sim = qcircuit_sim.depth()
             transpiled_width_sim = qcircuit_sim.num_qubits
             transpiled_depth_real = qcircuit_real.depth()
-            transpiled_width_real = qcircuit_real.num_qubits
+            transpiled_width_real = len(active_qubits_real)
 
             location_sim = run(circuit=qcircuit_sim, backend=sim_name, token=token, shots=shots,
                                benchmark_id=benchmark_id,
@@ -69,24 +77,41 @@ def randomize(qpu_name, num_of_qubits, shots, min_depth_of_circuit, max_depth_of
 
 def analyse():
     benchmarks = Benchmark.query.all()
-    string = ''
+    list = []
     for i in range(0, len(benchmarks), 2):
-        if benchmarks[i].complete and benchmarks[i + 1].complete and benchmarks[i].benchmark_id == benchmarks[i + 1].benchmark_id:
+        if benchmarks[i].complete and benchmarks[i + 1].complete and benchmarks[i].benchmark_id == benchmarks[
+            i + 1].benchmark_id:
             counts_sim = json.loads(benchmarks[i].counts)
             counts_real = json.loads(benchmarks[i + 1].counts)
-            print('before sim: ', counts_sim)
-            print('before real: ', counts_real)
-            for key in counts_sim.keys():
-                counts_sim[key] = counts_sim[key] / benchmarks[i].shots
-                if key not in counts_real.keys():
-                    counts_real[key] = 0
-            print('first sim: ', counts_sim)
-            print('first real: ', counts_real)
-            for key in counts_real:
-                counts_real[key] = counts_real[key] / benchmarks[i + 1].shots
-                if key not in counts_sim.keys():
-                    counts_sim[key] = 0
-            print('second sim: ', counts_sim)
-            print('second real: ', counts_real)
-        string = string + ' ' + str(benchmarks[i].benchmark_id) + ' ' + str(benchmarks[i + 1].benchmark_id)
-    return string
+            prb_sim = json.loads(benchmarks[i].counts)
+            prb_real = json.loads(benchmarks[i+1].counts)
+            shots = benchmarks[i+1].shots
+            for key in prb_sim.keys():
+                prb_sim[key] = prb_sim[key] / benchmarks[i].shots
+                if key not in prb_real.keys():
+                    prb_real[key] = 0
+            for key in prb_real:
+                prb_real[key] = prb_real[key] / benchmarks[i + 1].shots
+                if key not in prb_sim.keys():
+                    prb_sim[key] = 0
+            # exp_value_sim = analysis.calc_expected_value(prb_sim)
+            # exp_value_real = analysis.calc_expected_value(prb_real)
+            # sd_sim = analysis.calc_standard_deviation(prb_sim, exp_value_sim)
+            # sd_real = analysis.calc_standard_deviation(prb_real, exp_value_real)
+            perc_error = analysis.calc_percentage_error(counts_sim, counts_real)
+            correlation = analysis.calc_correlation(counts_sim.copy(), counts_real.copy(), shots)
+            intersection = analysis.calc_intersection(counts_sim.copy(), counts_real.copy(), shots)
+            list.append({"Benchmark " + str(benchmarks[i].benchmark_id): {
+                "Transpiled Depth": benchmarks[i+1].transpiled_depth,
+                "Transpiled Width": benchmarks[i+1].transpiled_width,
+                "Counts Sim": counts_sim,
+                # "Expected Value Sim": exp_value_sim,
+                # "Standard Deviation Sim": sd_sim,
+                "Counts Real": counts_real,
+                # "Expected Value Real": exp_value_real,
+                # "Standard Deviation Real": sd_real,
+                "Percentage Error": perc_error,
+                "Intersection": intersection,
+                "Correlation": correlation}
+            })
+    return list
