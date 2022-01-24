6d3a1b2cc7abe0f2d5703c2d7f22db64cd7aa7ad
salmma <marie.salm@iaas.uni-stuttgart.de>
2022-01-24 11:02:49 +0100

    update tests and test flask workflow

--- test/test_benchmarks.py
+++ test/test_benchmarks.py
@@ -47,9 +47,9 @@ class BenchmarksTestCase(unittest.TestCase):
         b2 = Benchmark(id="1")
         db.session.add(b1)
         db.session.add(b2)
-        b1.backend = json.dumps('ibmq_qasm_simulator')
+        b1.backend = 'ibmq_qasm_simulator'
         b1.benchmark_id = 0
-        b2.backend = json.dumps('ibmq_athens')
+        b2.backend = 'ibmq_athens'
         b2.benchmark_id = 0
         db.session.commit()
 
@@ -59,21 +59,25 @@ class BenchmarksTestCase(unittest.TestCase):
         db.session.add(b1)
         db.session.add(b2)
         b1.shots = 1024
-        b1.backend = json.dumps('ibmq_qasm_simulator')
+        b1.backend = 'ibmq_qasm_simulator'
         b1.original_depth = 1
         b1.original_width = 1
+        b1.original_number_of_multi_qubit_gates = 1
         b1.transpiled_depth = 10
         b1.transpiled_width = 1
+        b1.transpiled_number_of_multi_qubit_gates = 1
         b1.benchmark_id = 1
         b1.result = """{"text" : "I am complete !"}"""
         b1.counts = """{"00000": 1024}"""
         b1.complete = True
         b2.shots = 1024
-        b2.backend = json.dumps('ibmq_athens')
+        b2.backend = 'ibmq_athens'
         b2.original_depth = 1
         b2.original_width = 1
+        b2.original_number_of_multi_qubit_gates = 1
         b2.transpiled_depth = 10
         b2.transpiled_width = 1
+        b2.transpiled_number_of_multi_qubit_gates = 1
         b2.benchmark_id = 1
         b2.result = """{"text" : "I am complete !"}"""
         b2.counts = """{"10000": 1024}"""
@@ -99,38 +103,44 @@ class BenchmarksTestCase(unittest.TestCase):
         response = self.client.get('/qiskit-service/api/v1.0/benchmarks/%s' % benchmark_id)
         self.assertEqual(response.status_code, 200)
         benchmark = response.get_json()
-        self.assertIn("complete", benchmark[0])
-        self.assertIn("complete", benchmark[1])
-        self.assertEqual(False, benchmark[0]['complete'])
+        benchmarking_results = benchmark['benchmarking-results']
+        self.assertIn("complete", benchmarking_results[0])
+        self.assertEqual(False, benchmarking_results[0]['complete'])
+        self.assertIn("complete", benchmarking_results[1])
+        self.assertEqual(False, benchmarking_results[1]['complete'])
+        self.assertEqual(False, benchmark['benchmarking-complete'])
 
     def test_get_result_complete(self):
 
         benchmark_id = "1"
         response = self.client.get('/qiskit-service/api/v1.0/benchmarks/%s' % benchmark_id)
         self.assertEqual(response.status_code, 200)
-        print(response)
         benchmark = response.get_json()
-        print('hier', benchmark)
-        self.assertIn("complete", benchmark[0])
-        self.assertIn("complete", benchmark[1])
-        self.assertEquals("ibmq_qasm_simulator", benchmark[0]['backend'])
-        self.assertEquals(1, benchmark[0]['benchmark-id'])
-        self.assertEquals({"00000": 1024}, benchmark[0]['counts'])
-        self.assertEquals(1, benchmark[0]['original-depth'])
-        self.assertEquals(1, benchmark[0]['original-width'])
-        self.assertEquals(10, benchmark[0]['transpiled-depth'])
-        self.assertEquals(1, benchmark[0]['transpiled-width'])
-        self.assertEquals(1024, benchmark[0]['shots'])
-        self.assertEqual(True, benchmark[0]['complete'])
+        benchmarking_results = benchmark['benchmarking-results']
+        self.assertIn("complete", benchmarking_results[0])
+        self.assertIn("complete", benchmarking_results[1])
+        self.assertEquals("ibmq_qasm_simulator", benchmarking_results[0]['backend'])
+        self.assertEquals(1, benchmarking_results[0]['benchmark-id'])
+        self.assertEquals({"00000": 1024}, benchmarking_results[0]['counts'])
+        self.assertEquals(1, benchmarking_results[0]['original-depth'])
+        self.assertEquals(1, benchmarking_results[0]['original-width'])
+        self.assertEquals(1, benchmarking_results[0]['original-number-of-multi-qubit-gates'])
+        self.assertEquals(10, benchmarking_results[0]['transpiled-depth'])
+        self.assertEquals(1, benchmarking_results[0]['transpiled-width'])
+        self.assertEquals(1, benchmarking_results[0]['transpiled-number-of-multi-qubit-gates'])
+        self.assertEquals(1024, benchmarking_results[0]['shots'])
+        self.assertEqual(True, benchmarking_results[0]['complete'])
 
-        self.assertEquals(1, benchmark[1]['benchmark-id'])
-        self.assertEquals({"10000": 1024}, benchmark[1]['counts'])
-        self.assertEquals(1, benchmark[1]['original-depth'])
-        self.assertEquals(1, benchmark[1]['original-width'])
-        self.assertEquals(10, benchmark[1]['transpiled-depth'])
-        self.assertEquals(1, benchmark[1]['transpiled-width'])
-        self.assertEquals(1024, benchmark[1]['shots'])
-        self.assertEqual(True, benchmark[1]['complete'])
+        self.assertEquals(1, benchmarking_results[1]['benchmark-id'])
+        self.assertEquals({"10000": 1024}, benchmarking_results[1]['counts'])
+        self.assertEquals(1, benchmarking_results[1]['original-depth'])
+        self.assertEquals(1, benchmarking_results[1]['original-width'])
+        self.assertEquals(1, benchmarking_results[1]['original-number-of-multi-qubit-gates'])
+        self.assertEquals(10, benchmarking_results[1]['transpiled-depth'])
+        self.assertEquals(1, benchmarking_results[1]['transpiled-width'])
+        self.assertEquals(1, benchmarking_results[1]['transpiled-number-of-multi-qubit-gates'])
+        self.assertEquals(1024, benchmarking_results[1]['shots'])
+        self.assertEqual(True, benchmarking_results[1]['complete'])
 
 
 if __name__ == "__main__":
